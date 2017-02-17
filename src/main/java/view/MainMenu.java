c6cd1427bbbc760c855a0cb3b86658be84cb38fb
MaryiaChuhunova <Maryia_Chuhunova@epam.com>
2017-02-17 11:20:24 +0400

    Update MainMenu.java

--- src/main/java/view/MainMenu.java
+++ src/main/java/view/MainMenu.java
@@ -1,5 +1,8 @@
 package view;
 
+import controller.Controller;
+import controller.ControllerException;
+
 import java.util.Scanner;
 
 /**
@@ -15,23 +18,20 @@ public class MainMenu {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        AddReleaseMenu add = new AddReleaseMenu();
-        SearchMenu search = new SearchMenu();
-        String response = "";
+        String userResponse = "";
+        Controller controller = new Controller();
 
-        while (!response.equals("exit")) {
+
+        while (!userResponse.equals("exit")) {
             System.out.println("Type 'add' if you want to add release. \n Type 'search' if you want to search for release. " +
                     "\n type 'exit' if you want to finish");
-            response = sc.nextLine();
-            if (response.equals("add")) {
-                add.menu();
-            } else if (response.equals("search")) {
-                search.menu();
-            } else if (response.equals("exit")){
-                System.out.println("Bye");
-            } else {
-                System.out.println("Unknown instruction");
+            userResponse = sc.nextLine();
+            try {
+                System.out.println(controller.executeCommand(userResponse));
+            } catch (ControllerException ce) {
+                System.out.println(ce.getMessage());
             }
+
         }
     }
 }
